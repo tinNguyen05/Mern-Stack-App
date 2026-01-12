99acb13f94dcfa3f38e8acc973a122fd112c114c
Trong_Tin <tintinthanhthanh@gmail.com>
2026-01-12 08:05:14 +0700

    chore: update .gitignore and include missing project file

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,6 +1,8 @@
 const User = require("../models/user");
-const bcrypt = require("bcrypt");
+// Thay vì: const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+
 
 const userSignUp = async (req, res) => {
     try {
